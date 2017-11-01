39a09243b59204d1f5d6c36b591484b09b646288
Kranthi Kavuri <kranthik123@users.noreply.github.com>
2017-10-31 23:36:07 -0700

    updated deploy.php
    <?php
    // Use in the "Post-Receive URLs" section of your GitHub repo.
    if ( $_POST['payload'] ) {
      shell_exec( 'cd /var/www/html/ && git reset --hard HEAD && git pull' );
    }
    ?>

--- deploy.php
+++ deploy.php
@@ -1,2 +1,6 @@
-<?php `git pull`;
-
+<?php
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /var/www/html/ && git reset --hard HEAD && git pull' );
+}
+?>
